a2bd524b96f545dba1da444e4de857013cafef7b
otthonleao <otthonleao@hotmail.com>
2021-07-03 13:08:54 -0300

    correcao ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,31 +6,25 @@
 /*   By: orodrigo <orodrigo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 03:23:19 by orodrigo          #+#    #+#             */
-/*   Updated: 2021/06/16 15:19:13 by orodrigo         ###   ########.fr       */
+/*   Updated: 2021/07/03 13:01:08 by orodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(char *s1, char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	str1;
-	unsigned char	str2;
+	size_t	i;
 
-	i = 0;
 	if (n == 0)
 		return (0);
-	else
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		while ((i < (n - 1))
-			&& (s1[i] == s2[i])
-			&& (s1[i] != '\0'))
-		{
+		if (i < (n - 1))
 			i++;
-		}
-		str1 = s1[i];
-		str2 = s2[i];
+		else
+			return (0);
 	}
-	return (str1 - str2);
+	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
