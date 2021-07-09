b54b97772ea881430ef12828af1251c35d5f8051
otthonleao <otthonleao@hotmail.com>
2021-07-09 16:19:43 -0300

    adjustment on ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: orodrigo <orodrigo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 19:35:07 by orodrigo          #+#    #+#             */
-/*   Updated: 2021/07/09 15:50:50 by orodrigo         ###   ########.fr       */
+/*   Updated: 2021/07/09 16:17:37 by orodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	if (!s || !f)
 		return ;
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		(*f)(i, s + i);
+		f(i, s + i);
 		++i;
 	}
 }
